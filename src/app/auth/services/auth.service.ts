12de6255f29b91374fb40b3b95882b8a5dc7d328
panxingyu.icer <panxingyu.icer@bytedance.com>
2021-04-26 14:23:42 +0800

    feat: add toast when login and register

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { of as observableOf } from 'rxjs';
+import {
+  NbComponentStatus,
+  NbGlobalPhysicalPosition,
+  NbToastrService,
+} from '@nebular/theme';
 
 @Injectable()
 export class AuthService {
 
   private _authUrl = '/api/auth/';
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router,
+              private toastrService: NbToastrService) {}
 
   fetchUser() {
     const token = localStorage.getItem('token');
@@ -28,10 +34,17 @@ export class AuthService {
         // console.log(res);
         localStorage.setItem('token', JSON.stringify(res.token));
         localStorage.setItem('user', JSON.stringify(res.user));
-        this.router.navigateByUrl('/pages/search').then(r => {});
+        this.showToast('success', 'Register success', 'You will be redirected to search page.');
+        setTimeout(
+          function () {},
+          1500);
+        this.router.navigateByUrl('/pages/search').then(r => {
+          this.showToast('warning', 'Navigate failed', 'Please go to the login page.');
+        });
       },
       err => {
         // console.log(err);
+        this.showToast('warning', 'Register failed', 'Your email may have been registered.');
       },
     );
   }
@@ -42,11 +55,34 @@ export class AuthService {
         // console.log(res);
         localStorage.setItem('token', JSON.stringify(res.token));
         localStorage.setItem('user', JSON.stringify(res.user));
-        this.router.navigateByUrl('/pages/search').then(r => {});
+        this.showToast('success', 'Log success', 'You will be redirected to search page.');
+        setTimeout(
+          function () {},
+          1500);
+        this.router.navigateByUrl('/pages/search').then(r => {
+          this.showToast('warning', 'Navigate failed', 'Please refresh the page.');
+        });
       },
       err => {
         // console.log(err);
+        this.showToast('warning', 'Log failed', 'Please check your email and password.');
       },
     );
+  }
+
+  private showToast(type: NbComponentStatus, title: string, body: string) {
+    const config = {
+      status: type,
+      destroyByClick: true,
+      duration: 2000,
+      hasIcon: false,
+      position: NbGlobalPhysicalPosition.TOP_RIGHT,
+      preventDuplicates: true,
+    };
+    const titleContent = title ? `${title}` : '';
+    this.toastrService.show(
+      body,
+      `${titleContent}`,
+      config);
   }
 }
